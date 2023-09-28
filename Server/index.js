2796ee55713d38fdb989d4e26fd9ed1622e894d5
AgoraNgelale <agorangelale@icloud.com>
2023-09-27 22:00:35 -0700

    fixing CORS 9

--- Server/index.js
+++ Server/index.js
@@ -45,6 +45,8 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://agora-pedia-client.vercel.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
   
